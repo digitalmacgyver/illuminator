6fd2dde266f7d5d2aeae91df861f2c3655eff175
Matt Hayward <matt@viblio.com>
2022-11-25 15:52:53 -0800

    Updating settings.py

--- illuminator/wsgi.py
+++ illuminator/wsgi.py
@@ -33,6 +33,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "illuminator.settings")
 # application = HelloWorldApplication(application)
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = get_wsgi_application()
